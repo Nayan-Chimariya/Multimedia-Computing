c5c528c84a6d9164c062d6dd5422ca07b668693b
nayan chimariya <cnayan789@gmail.com>
2023-09-03 07:48:50 +0545

    ignored generated mp3

--- Lab_4/Text_to_speech.py
+++ Lab_4/Text_to_speech.py
@@ -1,13 +1,23 @@
 import pyttsx3 
 
-#* Initializing the engine for text to speech
-tts = pyttsx3.init()
+def text_to_speech(text,rate=0):  
+    """Generates Speech for your text
 
-tts.say("Hello my name is nayan")
+    Args:
+        text (str): Enter the text for speech generation
+        rate (int, optional): Enter speech rate. Defaults to 0.
+    """
+    #* Initializing the engine for text to speech
+    tts = pyttsx3.init()
+    default_rate = tts.getProperty('rate')
+    tts.setProperty('rate',default_rate+rate)
+    tts.say(text)
 
-#TODO tts.save_to_file('Hello my name is nayan' , 'test.mp3')
+    #TODO tts.save_to_file('Hello my name is nayan' , 'test.mp3')
 
-tts.runAndWait() 
+    tts.runAndWait() 
 
 #* Documentation in further examples 
 #* [https://pyttsx3.readthedocs.io/en/latest/engine.html#examples]
+
+text_to_speech("Hello World this is text to speech",200)
